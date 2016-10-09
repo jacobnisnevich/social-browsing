3f0bbaeadc6c076ed980a0aa10a42e168c4037b5
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-10-08 20:12:17 -0700

    display date correctly

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $('document').ready(function() {
 		$.get('get_browsing_data', function(data) {
 			JSON.parse(data).forEach(function(browsingDataRow) {
 				$('#browsing-list').empty().append('<div>' +
-					browsingDataRow.url + ' ' + browsingDataRow.date +
+					browsingDataRow.url + ' ' + (new Date(browsingDataRow.date).toLocaleString()) +
 				'</div>');
 			});
 		});

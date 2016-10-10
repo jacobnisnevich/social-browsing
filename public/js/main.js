46df510a924723b74a2526e4cc160896cfff94f2
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-10-09 23:31:50 -0700

    styling to frontend

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,25 @@ $('document').ready(function() {
 	window.setInterval(function() {
 		$.get('get_browsing_data', function(data) {
 			JSON.parse(data).forEach(function(browsingDataRow) {
-				$('#browsing-list').empty().append('<div>' +
+				$('#browsing-list').empty();
+				.append('<div>' +
 					browsingDataRow.url + ' ' + (new Date(parseInt(browsingDataRow.date)).toLocaleString()) +
 				'</div>');
 			});
 		});
 	}, 500);
 });
+
+function createBrowsingItem(url, username, date) {
+	$('#browsing-list').append(
+		'<div class="browsing-item">' + 
+			'<div class="browsing-iframe-container">' + 
+				'<iframe src="' + url + '" target="_blank"></iframe>' + 
+			'</div>' + 
+			'<div class="browsing-description-container">' + 
+				'<div class="browsing-username">' + '</div>' + 
+				'<div class="browsing-timestamp">' + date + '</div>' + 
+			'</div>' + 
+		'</div>'
+	);
+}
